7becfab8bddd522dce1cd0da2808a4948bb69b44
ahargunyllib <billy.bpm03@gmail.com>
2023-11-26 19:42:32 +0700

    finishing this project

--- index.js
+++ index.js
@@ -15,18 +15,41 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api", function (req, res) {
+	const date = new Date();
+	const unix = date.getTime()
+	const utc = date.toUTCString()
+
+	res.json({
+		unix: unix,
+		utc: utc
+	})
+})
+
+app.get("/api/:query", function (req, res) {
+	const query = !isNaN(req.params.query) ? parseInt(req.params.query) : req.params.query
+	const date = new Date(query)
+	if (date === 'Invalid Date'){
+		res.json({ error : 'Invalid Date'})
+	} else {
+		const unix = date.getTime()
+		const utc = date.toUTCString()
+	
+		res.json({
+			unix: unix,
+			utc: utc
+		})
+	}
 });
 
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+	console.log('Your app is listening on port ' + listener.address().port);
 });
